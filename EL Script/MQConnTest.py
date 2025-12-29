e26f1a005f354e6ce993a1bc46d79360af3607ee
Bradley Alojado <alojadobradley123@gmail.com>
2025-12-29 14:57:29 +0800

    removed conntest

--- EL Script/MQConnTest.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine("mysql+pymysql://root:Password@localhost/ecomm")
-
-try:
-    engine.connect()
-    print("Connected!")
-except Exception as e:
-    print("Connection error:", e)
